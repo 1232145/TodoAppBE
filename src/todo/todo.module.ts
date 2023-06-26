8da9a51f8b40458bcf83a785768f1df1fb2dc997
Huy Ngo <ngoviduchuy123@gmail.com>
2023-06-26 23:24:49 +0700

    midware

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TodoController } from './controllers/todo/todo.controller';
 import { TodoService } from './services/todo/todo.service';
+import { ValidateTodo } from './middlewares/validate-todo,middleware';
+
 
 @Module({
   controllers: [TodoController],
   providers: [TodoService]
 })
-export class TodoModule {}
+export class TodoModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ValidateTodo).forRoutes(
+      {
+        path: 'todo/:id',
+        method: RequestMethod.GET,
+      }
+    )
+  }
+}
