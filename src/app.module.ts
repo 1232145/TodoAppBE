b958dde19bd9566747e3ff161fab8a1900f4645e
Huy Ngo <ngoviduchuy123@gmail.com>
2023-06-26 20:17:09 +0700

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
